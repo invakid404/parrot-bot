dead804c35a8063608f91a11219144f381903510
invakid404 <invakid404@riseup.net>
2022-08-24 19:31:00 +0300

    chore: format with clang-format

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
+#include <concord/discord.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <concord/discord.h>
 
 void on_ready(struct discord*, const struct discord_ready* event) {
-	printf("Client is ready: %s#%s\n", event->user->username, event->user->discriminator);
+    printf("Client is ready: %s#%s\n", event->user->username,
+           event->user->discriminator);
 }
 
 int main(void) {
-	char* bot_token = getenv("PARROT_BOT_TOKEN");
+    char* bot_token = getenv("PARROT_BOT_TOKEN");
 
-	struct discord* client = discord_init(bot_token);
-	discord_set_on_ready(client, &on_ready);
+    struct discord* client = discord_init(bot_token);
+    discord_set_on_ready(client, &on_ready);
 
-	return discord_run(client);
+    return discord_run(client);
 }
