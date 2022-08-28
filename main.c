dead3e1c9fc7aa66581b7c62d1e7f6cc38d8cc95
invakid404 <invakid404@riseup.net>
2022-08-29 01:11:45 +0300

    feat: implement regex matching via libpcre2
    This was more painful than it should have been. Thank god for their
    demo program.
    
    The `regex_match` function was shamelessly stolen from
    https://github.com/PCRE2Project/pcre2/blob/master/src/pcre2demo.c.

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
+#include <assert.h>
 #include <concord/discord.h>
 #include <signal.h>
 #include <sqlite3.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "vector.h"
+
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include <pcre2.h>
+
 #include "init_sql.h"
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
@@ -15,6 +21,107 @@
 
 sqlite3* g_database;
 struct discord* g_client;
+pcre2_code* g_url_regex;
+
+struct vector* regex_match(pcre2_code* regex,
+                           PCRE2_SPTR subject,
+                           size_t subject_length) {
+#define APPEND_RESULT(i)                                                \
+    {                                                                   \
+        PCRE2_SPTR substring_start = subject + matches_vector[2 * (i)]; \
+        size_t substring_length =                                       \
+            matches_vector[2 * (i) + 1] - matches_vector[2 * (i)];      \
+        char* substring = calloc(1, subject_length + 1);                \
+        strncpy(substring, (char*)substring_start, substring_length);   \
+        vector_push(result_vector, substring);                          \
+    }
+
+    pcre2_match_data* match_data =
+        pcre2_match_data_create_from_pattern(regex, NULL);
+
+    int rc =
+        pcre2_match(regex, subject, subject_length, 0, 0, match_data, NULL);
+
+    if (rc < 0) {
+        pcre2_match_data_free(match_data);
+
+        return NULL;
+    }
+
+    struct vector* result_vector = vector_create();
+
+    PCRE2_SIZE* matches_vector = pcre2_get_ovector_pointer(match_data);
+
+    for (int i = 0; i < rc; i += 2) {
+        APPEND_RESULT(i)
+    }
+
+    int crlf_is_newline;
+    size_t option_bits;
+    uint32_t newline;
+
+    (void)pcre2_pattern_info(regex, PCRE2_INFO_ALLOPTIONS, &option_bits);
+    int utf8 = (option_bits & PCRE2_UTF) != 0;
+
+    (void)pcre2_pattern_info(regex, PCRE2_INFO_NEWLINE, &newline);
+    crlf_is_newline = newline == PCRE2_NEWLINE_ANY ||
+                      newline == PCRE2_NEWLINE_CRLF ||
+                      newline == PCRE2_NEWLINE_ANYCRLF;
+
+    for (;;) {
+        uint32_t options = 0;
+        PCRE2_SIZE start_offset = matches_vector[1];
+
+        if (matches_vector[0] == matches_vector[1]) {
+            if (matches_vector[0] == subject_length) {
+                break;
+            }
+
+            options = PCRE2_NOTEMPTY_ATSTART | PCRE2_ANCHORED;
+        }
+
+        rc = pcre2_match(regex, subject, subject_length, start_offset, options,
+                         match_data, NULL);
+
+        if (rc == PCRE2_ERROR_NOMATCH) {
+            if (options == 0) {
+                break;
+            }
+
+            matches_vector[1] = start_offset + 1;
+            if (crlf_is_newline && start_offset < subject_length - 1 &&
+                subject[start_offset] == '\r' &&
+                subject[start_offset + 1] == '\n') {
+                matches_vector[1] += 1;
+            } else if (utf8) {
+                while (matches_vector[1] < subject_length) {
+                    if ((subject[matches_vector[1]] & 0xc0) != 0x80) {
+                        break;
+                    }
+
+                    matches_vector[1] += 1;
+                }
+            }
+
+            continue;
+        }
+
+        if (rc < 0) {
+            pcre2_match_data_free(match_data);
+            vector_destroy(result_vector);
+
+            return NULL;
+        }
+
+        for (int i = 0; i < rc; i += 2) {
+            APPEND_RESULT(i)
+        }
+    }
+
+    pcre2_match_data_free(match_data);
+
+    return result_vector;
+}
 
 void on_ready(struct discord* client, const struct discord_ready* event) {
     u64snowflake application_id = event->application->id;
@@ -336,6 +443,8 @@ void cleanup(void) {
 
     discord_cleanup(g_client);
     ccord_global_cleanup();
+
+    pcre2_code_free(g_url_regex);
 }
 
 int main(void) {
@@ -350,6 +459,15 @@ int main(void) {
     if (database_path == NULL) {
         database_path = "parrot_bot.db";
     }
+
+    PCRE2_SPTR url_pattern = (PCRE2_SPTR) "(?:https?:\\/\\/)\\S+";
+
+    int error_number;
+    PCRE2_SIZE error_offset;
+
+    g_url_regex = pcre2_compile(url_pattern, PCRE2_ZERO_TERMINATED, 0,
+                                &error_number, &error_offset, NULL);
+    assert(g_url_regex != NULL);
 
     int result = sqlite3_open(database_path, &g_database);
     if (result != SQLITE_OK) {
